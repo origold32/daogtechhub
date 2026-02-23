3ca7a3c79dc6b1c46a55a1ff94f58f734cb59933
origold32 <muhibudeen.oriola@gmail.com>
2026-02-23 08:21:49 +0100

    displaying the HeroSection and ProductSection image on small screen

--- app/(landing)/components/ProductSection.tsx
+++ app/(landing)/components/ProductSection.tsx
@@ -94,7 +94,7 @@ export const ProductSection = forwardRef<HTMLElement, ProductSectionProps>(
           style={{ minHeight: "100vh" }}
         >
           {/* Left: text */}
-          <div className="w-full lg:w-[40vw] bg-red-500 lg:bg-transparent pt-28 pb-16 lg:py-0">
+          <div className="w-full lg:w-[40vw] bg-green-500 lg:bg-transparent pt-32 pb-16 lg:py-0">
             <motion.h2
               variants={textVariants}
               initial="hidden"
